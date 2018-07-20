4237a63d37e84b6bb1387c05c82e8f98da6bdc8f
Kristian Samuelsen <samuelsen.kristian@gmail.com>
2018-07-20 10:04:44 +0200

    materialize ui

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Footer } from 'react-materialize'
 import logo from './logo.svg';
 import './App.css';
 
@@ -13,6 +14,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Footer copyrights="&copy 2018 Naglfar">
+          <h5 className="black-text">Footer content</h5>
+        </Footer>
       </div>
     );
   }
