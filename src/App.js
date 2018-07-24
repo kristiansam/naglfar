952cecc1559205c37c26e15e6c84fe8bdb6889ea
Kristian Samuelsen <samuelsen.kristian@gmail.com>
2018-07-24 14:53:06 +0200

    New structure app.js

--- src/App.js
+++ src/App.js
@@ -1,51 +1,65 @@
 import React, { Component } from 'react';
-import { Navbar, NavItem, Icon, Row, Footer, Section, Container } from 'react-materialize'
+import { Navbar, NavItem, Icon, Row, Footer, Section, Container, Button, Col } from 'react-materialize'
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="container">
-        <Navbar className="light-blue lighten-1" brand="logo" right>
-          <NavItem href='#'><Icon>menu</Icon></NavItem>
-          <NavItem href='#'><Icon>search</Icon></NavItem>
-        </Navbar>
-        <div className="section">
-          <Row></Row>
-          <div className="row">
-            <div class="col s12 m4">
-            <div class="icon-block">
-              <h2 class="center light-blue-text"><i class="material-icons">flash_on</i></h2>
-              <h5 class="center">Speeds up development</h5>
-
-              <p class="light">We did most of the heavy lifting for you to provide a default stylings that incorporate our custom components. Additionally, we refined animations and transitions to provide a smoother experience for developers.</p>
-            </div>
-          </div>
-
-          <div class="col s12 m4">
-            <div class="icon-block">
-              <h2 class="center light-blue-text"><i class="material-icons">group</i></h2>
-              <h5 class="center">User Experience Focused</h5>
-
-              <p class="light">By utilizing elements and principles of Material Design, we were able to create a framework that incorporates components and animations that provide more feedback to users. Additionally, a single underlying responsive system across all platforms allow for a more unified user experience.</p>
-            </div>
-          </div>
-
-          <div class="col s12 m4">
-            <div class="icon-block">
-              <h2 class="center light-blue-text"><i class="material-icons">settings</i></h2>
-              <h5 class="center">Easy to work with</h5>
-
-              <p class="light">We have provided detailed documentation as well as specific code examples to help new users get started. We are also always open to feedback and can answer any questions a user may have about Materialize.</p>
-            </div>
-          </div>
-          </div>
-        </div>
-        <Footer className="orange">
-
-        </Footer>
+      <div>
+        <Container>
+          <Navbar className="light-blue lighten-1" brand="logo" right>
+            <NavItem href='#'><Icon>menu</Icon></NavItem>
+            <NavItem href='#'><Icon>search</Icon></NavItem>
+          </Navbar>
+          <Section>
+            <Container>
+              <br />
+              <br />
+              <h1 className="header center orange-text">Good Omens</h1>
+              <Row className="center">
+                <h5 className="header col s12 light">
+                  A modern game of prediction.
+                </h5>
+              </Row>
+              <Row className="center">
+                <Button className="btn-large">Get started</Button>
+              </Row>
+            </Container>
+          </Section>
+          <Section>
+            <Container>
+              <Row>
+                <Col className="m6 s12">
+                  <div className="icon-block">
+                    <h2 className="center light-blue-text"><Icon>settings</Icon></h2>
+                    <h5 className="center">Pretty good start</h5>
+                    <p className="light">
+                      A bunch of svada text....
+                    </p>
+                  </div>
+                </Col>
+                <Col className="m6 s12">
+                  <div className="icon-block">
+                      <h2 className="center light-blue-text"><Icon>group</Icon></h2>
+                      <h5 className="center">Easy for everyone</h5>
+                      <p className="light">
+                        More svada text....
+                      </p>
+                    </div>
+                </Col>
+              </Row>
+            </Container>
+          </Section>
+          <Footer className="orange" copyrights="Jalla 2018 Good Omens">
+            <h5 className="white-text">Good Omens</h5>
+            <p className="grey-text text-lighten-4">
+              Predict game.
+            </p>
+          </Footer>
+        </Container>
       </div>
+
       // <div className="App">
       //   <header className="App-header">
       //     <img src={logo} className="App-logo" alt="logo" />
